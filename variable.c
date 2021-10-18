638fdd60fcaa0391d007d08559a7b4fe5059a782
Gramandha Wega Intyanto <47823515+anagdesa@users.noreply.github.com>
2021-10-18 23:27:40 +0700

    v1

---
+++ variable.c
@@ -0,0 +1,14 @@
+// Variable
+
+#include<stdio.h>
+
+int main(){
+
+    // contoh variable
+    char characterName[]= "Wega";  //string
+    int characterAge = 29;
+
+    // proses memanggil variabel ("tipe variabel", namaVariable)
+    printf("Nama saya: %s \n", characterName);
+    printf("Umur saya : %d\n", characterAge);
+}
